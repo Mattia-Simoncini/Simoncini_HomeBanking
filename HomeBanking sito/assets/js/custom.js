750acd78198ae967a1d5a1d2c1aa9f8393e637f4
Mattia Simoncini <65952532+Mattia-Simoncini@users.noreply.github.com>
2021-09-29 13:09:47 +0200

    Update custom.js

--- HomeBanking sito/assets/js/custom.js
+++ HomeBanking sito/assets/js/custom.js
@@ -12,5 +12,5 @@ $(document).ready(function () {
 });
 
 function enableEdit(x) {
-    document.getElementById("buttonEdit"+x).disabled = false;
+    document.getElementById("buttonEdit"+x).enabled = true;
 }
